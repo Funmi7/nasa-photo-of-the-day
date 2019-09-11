0a40e0441fe7a279d4e42b955c4e914608e7445e
Funmi7 <talabifunmilayo@gmail.com>
2019-09-11 12:59:49 +0100

    Created separate files for my planned components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./App.css";
+import TopContent from "./TopContent";
 
 function App() {
+
+  
   return (
     <div className="App">
-      <p>
+      {/* <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
-      </p>
+      </p> */}
+      <TopContent/>
     </div>
   );
 }
---
+++ src/TopContent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function TopContent() {
+    return(
+        <div>
+            <h1>NASA Photo Of The Day!</h1>
+            <p>Here is the Photo of the day from NASA</p>
+        </div>
+    )
+}
+    
+export default TopContent;
