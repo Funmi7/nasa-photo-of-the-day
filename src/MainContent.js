bd1d9191510499e045d2b1a6e5bff21fd9732dd2
Funmi7 <talabifunmilayo@gmail.com>
2019-09-12 07:49:00 +0100

    Added more css stylings

--- src/MainContent.js
+++ src/MainContent.js
@@ -6,11 +6,12 @@ const {title, date, description} = props;
 return (
     <div>
         <div className="title-date-container">
-        <h3>Title: {title}</h3>
-        <h3>Date:</h3>
-        </div>
-        <p>{date}</p>
-        <h3>Description of Image</h3>
+            <h3>Title: {title}</h3>
+            <div>
+                <p>Date: <span>{date}</span></p>
+            </div>
+            </div>
+        <h3 className="description-header">Description of Image</h3>
         <p>{description}</p>
     </div>
 )
